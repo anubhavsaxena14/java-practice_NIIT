10457d9fd087e0b3ddbf115385fc301ebac42c43
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2017-05-30 23:53:58 +0530

    Initial Commit

---
+++ Exception Handling/exception.java
@@ -0,0 +1,18 @@
+ import java.util.Scanner; 
+class exception1
+{
+	public static void main ( String ar[])
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);  
+	    System.out.println("Enter first num  ");  
+		a=sc.nextInt();  
+	    System.out.println("Enter sec number   ");  
+		b=sc.nextInt();  
+		c=a/b;
+		System.out.println(c);
+		System.out.println("END");
+	}
+}	
+	
+  
